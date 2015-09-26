344e33d03dfa72e6a395e7a392731baf6cf12163
YaoYao <ybai36@gatech.edu>
2015-09-26 17:50:16 -0400

    initial commmit

---
+++ d3StackedGraph.js
@@ -0,0 +1,101 @@
+// d3 stacked graph code
+  var color = d3.scale.category20();
+
+function drawStackedGraph() {
+  var n = topWords.length, // number of layers
+      m = hours, // number of samples per layer
+      stack = d3.layout.stack(),
+      layers = stack(d3.range(n).map(function(data) { return getGraphHeights(m, data); }));
+      console.log(layers);
+      yGroupMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y; }); }),
+      yStackMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
+
+  var margin = {top: 40, right: 10, bottom: 20, left: 10},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+      .domain(d3.range(m))
+      .rangeRoundBands([0, width], .08);
+
+  var y = d3.scale.linear()
+      .domain([0, yStackMax])
+      .range([height, 0]);
+
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .tickSize(0)
+      .tickPadding(6)
+      .orient("bottom");
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var layer = svg.selectAll(".layer")
+      .data(layers)
+    .enter().append("g")
+      .attr("class", "layer")
+      .style("fill", function(d, i) { return color(i);
+  });
+
+  var rect = layer.selectAll("rect")
+      .data(function(d) { return d; })
+      .enter().append("rect")
+      .attr("x", function(d) { return x(d.x); })
+      .attr("y", height)
+      .attr("width", x.rangeBand())
+      .attr("title", function(d, i, j) { return topWords[j];})
+      .on('mouseover', function(d,i,j) {
+        showWord(j,topWords[j]);
+      });
+  rect.append("text")
+      .text(function(d,i,j) {return topWords[j];})
+
+  rect.transition()
+      .delay(function(d, i) { return i * 10; })
+      .attr("y", function(d) { return y(d.y0 + d.y); })
+      .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+
+    rect.attr("class", function(d) {
+        return "hour bordered" + "color-" + colorScale(d.value).substring(1);
+      })
+      .on("mouseover", function(d,i) {
+        svg.selectAll("rect.color-" + colors[i].substring(1)).style("stroke", "blue");
+      })
+      .on("mouseout", function(d,i) {
+        svg.selectAll("rect.color-" + colors[i].substring(1)).style("stroke", "white");
+      });
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .attr("title", function(d, i) { return topWords[i]; });
+
+
+
+}
+function showWord(j,word) {
+  $('#showWord').html(word).css('color', color(j));
+}
+// Inspired by Lee Byron's test data generator.
+function getGraphHeights(bucket, keyIndex) {
+
+  var a = [];
+  for (i = 0; i < bucket; i++) {
+    word = topWords[keyIndex];
+    if (sortedWords[i][word]) {
+      a.push({"x":i, "y":sortedWords[i][word]});
+    } else {
+      a.push({"x":i, "y":0});
+    }
+  }
+
+  return a;
+
+  
+}
---
+++ tweetCounter.js
@@ -0,0 +1,90 @@
+var hours = 18;
+
+$.getJSON("tweets10pm10am.json", 
+	function( data ) {
+		console.log("blegh");
+		wordCount(data, hours);
+	}
+
+);
+
+// this function puts the words grabbed into a bucket and sorts them
+function wordCount(data, numBuckets) {
+	wordCounter = [];
+	sortedWords = [];
+	totalWordCount = {};
+	keyArr = {};
+	for(i=0; i<numBuckets; i++) {
+		wordCounter[i] = {};
+		sortedWords[i] = {};
+	}
+	for(i in data) {
+		tweet = data[i]
+		myBucket = Math.floor((1443300192 - parseInt(tweet["timestamp"]))/3600);
+		console.log(myBucket)
+		for(j in tweet.keywords) {
+			myWord = tweet.keywords[j];
+			if (totalWordCount[myWord]) {
+				totalWordCount[myWord]++;
+			} else {
+				totalWordCount[myWord] = 1;
+			}
+			if(wordCounter[myBucket][myWord]) {
+				wordCounter[myBucket][myWord]++;
+			} else {
+				wordCounter[myBucket][myWord] = 1;
+			}
+		}
+	}
+	for (bucket = 0; bucket < numBuckets; bucket++) {
+		keys = Object.keys(wordCounter[bucket]).sort(function(a,b) {
+			return wordCounter[bucket][b] - wordCounter[bucket][a];
+		});
+		//console.log(keys);
+		keyArr[bucket] = keys;
+	}
+	//console.log(keyArr);
+	for (index in keyArr) {
+		keys = keyArr[index];
+		counter = 0;
+		for (key in keys) {
+			word = keys[key];
+			if (counter < 20) {
+				//$("body").append(word +"->"+wordCounter[index][word] + "  ");
+				counter++;
+				sortedWords[index][word] = wordCounter[index][word];
+			} else {
+				break;
+			}
+		}
+		//$("body").append("<br><br>");
+	}
+	//$("body").append("topWords: ");
+	topWordKeys = Object.keys(totalWordCount).sort(function(a,b) {
+		return totalWordCount[b]-totalWordCount[a];
+	});
+	//console.log(topWordKeys)
+	topWords = [];
+	counter = 0;
+	for(topwordkey in topWordKeys) {
+		word = topWordKeys[topwordkey];
+		if (counter < 20) {
+			counter++;
+			//$("body").append(word + "->"+totalWordCount[word]);
+			topWords.push(word);
+		} else {
+			break;
+		}
+	}
+	console.log(topWords);
+	// keysSorted = Object.keys(wordCounter).sort(function(a,b) {
+	// 	return wordCounter[b] - wordCounter[a];
+	// });
+	
+	// for(index in keysSorted) {
+	// 	word = keysSorted[index]
+	// 	$('body').append("<p>" + word + "->" + wordCounter[word] + '</p>')
+	// }
+	drawStackedGraph();
+
+}
